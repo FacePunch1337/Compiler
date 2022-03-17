d4cd92996f6c260fa02e6af1ba4db685a996e474
FacePunch1337 <rezol1337@gmail.com>
2022-03-17 17:46:05 +0200

    Добавьте файлы проекта.

---
+++ compiler/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+using System;
+
+namespace compiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.tbFrameworke = new System.Windows.Forms.TextBox();
+            this.tbCodeArea = new System.Windows.Forms.RichTextBox();
+            this.tbDebugArea = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выходной файл";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(560, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "framework";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFilePath.Location = new System.Drawing.Point(118, 6);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.Size = new System.Drawing.Size(311, 20);
+            this.tbFilePath.TabIndex = 2;
+            this.tbFilePath.Text = "Test.exe";
+            this.tbFilePath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbFrameworke
+            // 
+            this.tbFrameworke.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFrameworke.Location = new System.Drawing.Point(613, 6);
+            this.tbFrameworke.Name = "tbFrameworke";
+            this.tbFrameworke.Size = new System.Drawing.Size(141, 20);
+            this.tbFrameworke.TabIndex = 3;
+            this.tbFrameworke.Text = "v4.0";
+            // 
+            // tbCodeArea
+            // 
+            this.tbCodeArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCodeArea.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbCodeArea.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbCodeArea.ForeColor = System.Drawing.Color.AliceBlue;
+            this.tbCodeArea.Location = new System.Drawing.Point(27, 44);
+            this.tbCodeArea.Name = "tbCodeArea";
+            this.tbCodeArea.Size = new System.Drawing.Size(746, 263);
+            this.tbCodeArea.TabIndex = 4;
+            this.tbCodeArea.Text = "\n";
+            this.tbCodeArea.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // tbDebugArea
+            // 
+            this.tbDebugArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDebugArea.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbDebugArea.Location = new System.Drawing.Point(27, 349);
+            this.tbDebugArea.Name = "tbDebugArea";
+            this.tbDebugArea.Size = new System.Drawing.Size(746, 68);
+            this.tbDebugArea.TabIndex = 5;
+            this.tbDebugArea.Text = "";
+            this.tbDebugArea.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(490, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbDebugArea);
+            this.Controls.Add(this.tbCodeArea);
+            this.Controls.Add(this.tbFrameworke);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "compile";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.TextBox tbFrameworke;
+        private System.Windows.Forms.RichTextBox tbCodeArea;
+        private System.Windows.Forms.RichTextBox tbDebugArea;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ compiler/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+
+namespace compiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", tbFrameworke.Text } });
+
+            CompilerParameters parameters = new CompilerParameters(new string[] { "mscorlib.dll", "System.Core.dll" }, tbFilePath.Text, true);
+
+            parameters.GenerateExecutable = true;
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, tbCodeArea.Text);
+            
+            if (results.Errors.HasErrors)
+            {
+                foreach(CompilerError error in results.Errors.Cast<CompilerError>())
+                {
+                    tbDebugArea.Text += $"Строка {error.Line}: {error.ErrorText}";
+                }
+                
+            }
+            else
+            {
+                tbDebugArea.Text = "[Сборка завершена]";
+
+                Process.Start($"{Application.StartupPath}/{tbFilePath.Text}");
+            }
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}  
